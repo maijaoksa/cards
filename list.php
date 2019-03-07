a7a7670c5d04a2e76da0524d9bc2c481b0a525d0
Alice <maija.oksa@outlook.com>
2019-03-07 22:52:41 +0200

    Revert "Adding feture/add branch"
    This reverts commit c5d1ffb581e6f78930e9a508891d92277bd957ec.
    
    Conflicts:
    	list.php

--- list.php
+++ list.php
@@ -3,6 +3,7 @@
 session_start();
 if($_SESSION['username'] == 'username') {
 <<<<<<< HEAD
+<<<<<<< HEAD
   include "connect.php";
   print("<html>");
   $conn = connect_db();
@@ -59,6 +60,33 @@ if($_SESSION['username'] == 'username') {
   } else {
    print("No access.");
  
+=======
+ include "connect.php";
+ print("<html>");
+ $conn = connect_db();
+ $sql = "SELECT * FROM Addresses";
+ $result = mysqli_query($conn, $sql);
+ if (mysqli_num_rows($result) > 0) {
+ // output data of each row
+ while($row = mysqli_fetch_assoc($result)) {
+ echo $row["id"] . " ";
+ echo $row["name"] . " ";
+ echo $row["street_address"] . " ";
+ echo $row["zip"] . " ";
+ echo $row["city"] . " ";
+ echo $row["state"] . " ";
+ echo $row["country"] . " ";
+ print("<br>");
+ }
+ } else {
+ echo "0 results";
+ }
+ print("</html>");
+ mysqli_close($conn);
+} else {
+ print("No access.");
+}
+>>>>>>> parent of c5d1ffb... Adding feture/add branch
 ?>
 
 
